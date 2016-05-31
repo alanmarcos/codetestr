e7ed33edc28bf1cbe3a7739a4aa33da99ad2d79f
Alan Marcos <alanlpu@gmail.com>
2016-05-30 23:16:01 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,125 @@
+			var App = function(){
+
+				public = {};
+
+				_divs = {
+					newsfeed: 	$('#newsfeed'),
+					friends: 	$('#friends'),
+					formStatus: $('#form-status')
+				}
+
+				_templates = {
+					newsfeed: 			$('#newsfeed-template').html(),
+					newsfeedPersonal: 	$('#newsfeed-template-personal').html(),
+					friends: 			$('#friends-template').html()
+				}
+
+				public.init = function(obj){
+					_updateNewsfeed();
+					_updateFriendsList();
+					_bindEvents();
+				};
+
+				function _bindEvents(){
+
+					_divs.formStatus.on('submit', function(){
+
+						var data = $(this).serialize();
+						_updateStatus(data);
+						return false;
+
+					});
+
+				};
+
+				function _updateNewsfeed(){
+
+					var deferred = $.Deferred();
+
+					$.ajax({
+						url: 'https://api.backand.com/1/objects/status',
+						dataType: 'json',
+						method: 'get',
+						headers: {
+							AnonymousToken: '7e507e02-3eaf-401d-b3a9-a33e823d632f'
+						},
+						success: function(data){
+							deferred.resolve(data);
+						}
+					});
+
+					deferred.done(function(data){
+
+						var template = Handlebars.compile(_templates.newsfeed);
+						_divs.newsfeed.html(template(data));
+
+					});
+
+				};
+
+				function _updateFriendsList(){
+
+					var deferred = $.Deferred();
+
+					$.ajax({
+						url: 'https://api.backand.com:443/1/objects/friends?pageSize=20&pageNumber=1',
+						dataType: 'json',
+						method: 'get',
+						headers: {
+							AnonymousToken: '7e507e02-3eaf-401d-b3a9-a33e823d632f'
+						},
+						success: function(data){
+							deferred.resolve(data);
+						}
+					});
+
+					deferred.done(function(data){ 
+
+						var template = Handlebars.compile(_templates.friends);
+						_divs.friends.html(template(data));
+
+					});
+
+				}
+
+				function _updateStatus(serializedString){
+
+					var deferred = $.Deferred();
+
+					$.ajax({
+						url: 'https://api.backand.com/1/objects/status',
+						method: 'post',
+						dataType: 'json',
+						data: serializedString,
+						headers: {
+							AnonymousToken: '7e507e02-3eaf-401d-b3a9-a33e823d632f',
+							"Content-Type": "application/json"
+						},
+						success: function(data){
+							deferred.resolve(data);
+						},
+						error: function(data){
+							var span = $('<span>').html(data.responseText).addClass('pull-right error');
+							_divs.formStatus.append(span);
+							setTimeout(function(){
+								_divs.formStatus.find('.error').remove();
+							}, 3500);
+						}
+					});
+
+					deferred.done(function(data){
+
+						var template = Handlebars.compile(_templates.newsfeedPersonal);
+						_divs.newsfeed.prepend(template(data));
+						_divs.formStatus[0].reset();
+						_updateFriendsList();
+
+					});
+
+				};
+
+				return public;
+
+			}();
+
+			App.init();
---
+++ readme.md
@@ -0,0 +1,9 @@
+# CaraLivro
+
+This is a facebook-like social network, except it's way damn simpler. This was built with jQuery and Handlebars.js.
+
+### Initializing
+  - Make sure you have your templates in a `<scripts type="text/template">` tag with their respective IDs (they're listed on the `_templates` object within the `App`.
+  - Make sure the IDs in the `_divs` object within the `App` are correct.
+  - `App.init()` will finish the job.
+
